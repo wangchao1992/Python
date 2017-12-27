bf87575633486b01c3a662e74f3c9913a8322c4b
王超 <wangchao2@sinorail.com>
2017-12-27 16:23:12 +0800

    no message

---
+++ day4-代码块.py
@@ -0,0 +1,14 @@
+# -*-coding:utf-8 -*-
+'''
+很多语言 都是以距离相近 的大括号（{ }），称为一组代码块
+if(isTrue){
+    xxxx
+    xxxx
+}
+在 Python 里 拥有相同缩进的代码行，称为一组代码块。
+'''
+# Python 里对缩进 要求是及其严格的。
+i = 5
+# 下面将发生错误，注意行首有一个空格， 把首行的空格删除再试试
+ print('Value is',i)
+print('I repeat, the value is', i)
