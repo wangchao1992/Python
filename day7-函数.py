51695eeb464bce104ce6acc52103d18e22f81878
王超 <wangchao2@sinorail.com>
2017-12-28 16:55:33 +0800

    函数知识点

---
+++ day7-函数.py
@@ -0,0 +1,63 @@
+# -*-coding:utf-8 -*-
+'''
+函数（Functions）是指可重复使用的程序片段。
+函数可以通过关键字  def  来定义。
+
+'''
+# -------------------------无参数  函数--------------------------
+def say_hello():
+    print('hello world')
+
+say_hello()
+# ---------------------------有参数 函数-------------------------------
+def say_hello_to(name):
+    print('hello ',name)
+say_hello_to('chao')
+# ---------------------------函数默认值------------------------------
+# 带有默认值 的参数，必须在参数列表的末尾。
+#  因为值是按参数所处的位置依次分配的。举例来说， def func(a, b=5)  是有效的，
+# 但  def func(a=5, b)  是无效的
+def say(name,age=10):
+    print('{}\'s age is {}. '.format(name,age))
+say('chao') # 第二个参数 age，不传默认是 10
+say('chao',23)
+# 也可以通过关键字 传递参数， 这样是不分先后顺序的。
+# 必须把没有默认值的参数都写全，否则会报错
+say(age=25,name='chao')
+#----------------------------可变参数---------------------------------
+'''
+有时你可能想定义的函数里面能够有任意数量的变量，也就是参数数量是可变的，这可以通过使用星号来实现
+
+'''
+print('{0:*^50}'.format('可变参数'))
+def total(a=5, *numbers, **phonebook):
+    print('a', a)
+    #遍历元组中的所有项目
+    for single_item in numbers:
+        print('single_item', single_item)
+    #遍历字典中的所有项目
+    for first_part, second_part in phonebook.items():
+        print(first_part,second_part)
+print(total(10,1,2,3,Jack=1123,John=2231,Inge=1560))
+'''
+它是如何工作的
+当我们声明一个诸如  *param  的星号参数时，从此处开始直到结束的所有位置参数
+（Positional Arguments）都将被收集并汇集成一个称为“param”的元组（Tuple）。
+类似地，当我们声明一个诸如  **param  的双星号参数时，从此处开始直至结束的所有关键字
+参数都将被收集并汇集成一个名为  param  的字典（Dictionary）。
+'''
+#----------------------------函数返回值------------------------------
+def maximum(x,y):
+    if x>y:
+        return x
+    elif x<y:
+        return y
+    else:
+        return 'The numbers are equal'
+print(maximum(5,-19))
+#再看一下这个例子
+def some_fun():
+    pass
+print(some_fun())
+# Python 中的  pass  语句用于指示一个没有内容的语句块
+# 每一个函数都在其末尾隐含了一句  return None  ，除非你写了你自己的  return  语句
---
+++ day7-函数变量作用域.py
@@ -0,0 +1,25 @@
+# -*-coding:utf-8 -*-
+#-------------- 局部变量 ------------------
+x = 50
+def func(x):
+    print('x is', x)
+    x = 2
+    print('Changed local x to', x)
+func(x)
+print('x is still', x)
+# 发现没有，在函数内部修改 x = 2 ,对外边的变量没有影响
+
+#-------------- 全局变量 ------------------
+print('{0:*^50}'.format('global'))
+x = 50
+def func(x):
+    print('x is', x)
+    global x1
+    global y, z
+    z=y=100 # 一次给多个变量赋值
+    x1=x
+    print('Changed x1 to', x)
+func(x)
+print('x1 is still', x1)
+print('y = {},z = {}'.format(y,z))
+#发现没有，  x1 ,y,z 定义在函数内部 ，在函数外部也可以调用
