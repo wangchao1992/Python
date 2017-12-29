27956baad57b662c31a593efad0ce527ca94dd88
王超 <wangchao2@sinorail.com>
2017-12-29 15:08:47 +0800

    no message

--- day8-modules.py
+++ day8-modules.py
@@ -26,3 +26,42 @@ if __name__ == '__main__':
     print('This program is being run by itself')
 else:
     print('I am being imported from another module')
+# 使用 dir（） 函数查看，一个类内部的变量及方法 ， 参数是要查看类的名字， 如果不传 是当前类
+a =5
+print(dir())
+del a
+print(dir())# 发现 a  不见了
+print(dir(sqrt))
+# 包
+'''
+现在，你必须开始遵守用以组织你的程序的层次结构。变量通常位于函数内部，函数与全局
+变量通常位于模块内部。如果你希望组织起这些模块的话，应该怎么办？这便是包
+（Packages）应当登场的时刻。
+包是指一个包含模块与一个特殊的  __init__.py  文件的文件夹，后者向 Python 表明这一文
+件夹是特别的，因为其包含了 Python 模块。
+建设你想创建一个名为“world”的包，其中还包含着 ”asia“、”africa“等其它子包，同时这些子
+包都包含了诸如”india“、”madagascar“等模块。
+'''
+# 下面是你会构建出的文件夹的结构：
+'''
+- <some folder present in the sys.path>/
+    - world/
+        - __init__.py
+        - asia/
+              - __init__.py
+              - india/
+                  - __init__.py
+                  - foo.py
+        - africa/
+              - __init__.py
+              - madagascar/
+                 - __init__.py
+                 - bar.py
+包是一种能够方便地分层组织模块的方式。你将在 标准库 中看到许多有关于此的实例。
+'''
+'''
+总结
+如同函数是程序中的可重用部分那般，模块是一种可重用的程序。包是用以组织模块的另一
+种层次结构。Python 所附带的标准库就是这样一组有关包与模块的例子。
+我们已经了解了如何使用这些模块并创建你自己的模块。
+'''
---
+++ day9-数据结构.py
@@ -0,0 +1,55 @@
+# -*-coding:utf-8 -*-
+'''
+数据结构（Data Structures）基本上人如其名——它们只是一种结构，能够将一些数据聚合
+在一起。换句话说，它们是用来存储一系列相关数据的集合。
+Python 中有四种内置的数据结构——列表（List）、元组（Tuple）、字典（Dictionary）和集
+合（Set）。我们将了解如何使用它们，并利用它们将我们的编程之路变得更加简单。
+个人总结：数据结构就是数组存储的一种结构。每种结构都有自己的特点
+'''
+# ----------------------list 列表-------------------
+# 列表 是存储一组 数据的 数据结构。 可对列表，添加 ，修改，删除， 因此 它相当灵活 ，是一种可变的序列
+shoplist = ['apple', 'mango', 'carrot', 'banana']
+shoplist.append('rice')#追加元素
+print(shoplist)
+shoplist.sort() #排序
+print(shoplist)
+del shoplist[0]# 删除列表第一个元素
+print(shoplist)
+shoplist.append(1)# 可以向列表中 添加 数字，list ，对象
+shoplist.append(['a',print()])
+print(shoplist) # 列表 也是一种序列 ，可以用 for 循环遍历
+for i in shoplist:
+    print(i)
+# ------------ Tuple 元组--------------
+# 元组 和列表 很相似， 但元组没有 修改， 添加，删除 元素的功能，所以他是不可变的序列
+tuple = ('python','elephant','penguin')
+print('tuple\'s len is ',len(tuple))#  输出元组的长度。
+new_zoo = 'monkey','camel',tuple
+print(new_zoo)
+print('Animals brought from old zoo are', new_zoo[2])
+print('Last animal brought from old zoo is', new_zoo[2][2])
+print('Number of animals in the new zoo is',
+len(new_zoo)-1+len(new_zoo[2]))
+# 遍历 元组里的内容
+for i in new_zoo:
+    print(i)
+print((2)) # 这是一个数字 2
+print((2,)) # 这是只有一个元素 （2） 的 元组，
+#------------------------- dict 字典 ------------------------
+# “ab”是地址（Address）簿（Book）的缩写
+ab = {
+'Swaroop': 'swaroop@swaroopch.com',
+'Larry': 'larry@wall.org',
+'Matsumoto': 'matz@ruby-lang.org',
+'Spammer': 'spammer@hotmail.com'
+}
+print("Swaroop's address is", ab['Swaroop'])
+# 删除一对键值—值配对
+del ab['Spammer']
+print('\nThere are {} contacts in the address-book\n'.format(len(ab)))
+for name, address in ab.items():
+    print('Contact {} at {}'.format(name, address))
+# 添加一对键值—值配对
+ab['Guido'] = 'guido@python.org'
+if 'Guido' in ab:
+    print("\nGuido's address is", ab['Guido'])
